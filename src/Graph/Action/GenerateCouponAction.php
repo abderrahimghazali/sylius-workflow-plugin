ce98b89ae3eaf445c8362ad0053f3458ff1b7d15
Abderrahim GHAZALI <ghazali.abderrahim1@gmail.com>
2026-03-29 08:31:52 +0200

    Auto-create promotion in GenerateCouponAction if it doesn't exist

--- src/Graph/Action/GenerateCouponAction.php
+++ src/Graph/Action/GenerateCouponAction.php
@@ -7,15 +7,20 @@ namespace Abderrahim\SyliusWorkflowPlugin\Graph\Action;
 use Abderrahim\SyliusWorkflowPlugin\Graph\WorkflowContext;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
+use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
 use Sylius\Component\Core\Model\PromotionCouponInterface;
+use Sylius\Component\Core\Model\PromotionInterface;
 use Sylius\Component\Promotion\Factory\PromotionCouponFactoryInterface;
 use Sylius\Component\Promotion\Repository\PromotionRepositoryInterface;
+use Sylius\Component\Resource\Factory\FactoryInterface;
 
 final class GenerateCouponAction implements ActionInterface
 {
     public function __construct(
         private readonly PromotionRepositoryInterface $promotionRepository,
         private readonly PromotionCouponFactoryInterface $couponFactory,
+        private readonly FactoryInterface $promotionFactory,
+        private readonly ChannelRepositoryInterface $channelRepository,
         private readonly EntityManagerInterface $entityManager,
         private readonly LoggerInterface $logger,
     ) {
@@ -31,12 +36,18 @@ final class GenerateCouponAction implements ActionInterface
         $promotionCode = $config['promotion'] ?? '';
         $usageLimit = (int) ($config['usage_limit'] ?? 1);
         $expiresInDays = (int) ($config['expires_in_days'] ?? 30);
+        $discount = (int) ($config['discount'] ?? 10);
+
+        if ($promotionCode === '') {
+            return ['success' => false, 'message' => 'No promotion code specified.'];
+        }
 
         try {
-            /** @var \Sylius\Component\Promotion\Model\PromotionInterface|null $promotion */
+            /** @var PromotionInterface|null $promotion */
             $promotion = $this->promotionRepository->findOneBy(['code' => $promotionCode]);
+
             if ($promotion === null) {
-                return ['success' => false, 'message' => sprintf('Promotion "%s" not found.', $promotionCode)];
+                $promotion = $this->createPromotion($promotionCode, $discount);
             }
 
             /** @var PromotionCouponInterface $coupon */
@@ -57,5 +68,36 @@ final class GenerateCouponAction implements ActionInterface
 
             return ['success' => false, 'message' => 'Coupon generation failed: ' . $e->getMessage()];
         }
+    }
+
+    private function createPromotion(string $code, int $discountPercent): PromotionInterface
+    {
+        /** @var PromotionInterface $promotion */
+        $promotion = $this->promotionFactory->createNew();
+        $promotion->setCode($code);
+        $promotion->setName('Workflow: ' . $code);
+        $promotion->setCouponBased(true);
+        $promotion->setPriority(0);
+        $promotion->setExclusive(false);
+
+        // Add to all channels
+        $channels = $this->channelRepository->findAll();
+        foreach ($channels as $channel) {
+            $promotion->addChannel($channel);
+        }
+
+        // Add percentage discount action
+        /** @var \Sylius\Component\Promotion\Model\PromotionActionInterface $action */
+        $action = new \Sylius\Component\Core\Model\PromotionAction();
+        $action->setType('order_percentage_discount');
+        $action->setConfiguration(['percentage' => $discountPercent / 100]);
+        $promotion->addAction($action);
+
+        $this->entityManager->persist($promotion);
+        $this->entityManager->flush();
+
+        $this->logger->info(sprintf('Workflow auto-created promotion "%s" with %d%% discount.', $code, $discountPercent));
+
+        return $promotion;
     }
 }
