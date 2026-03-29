7c44c25f8c7c97cbbb515403f8a62c832f371406
Abderrahim GHAZALI <ghazali.abderrahim1@gmail.com>
2026-03-29 08:16:03 +0200

    Extend AbstractController for flash messages and simplify controller

--- src/Controller/Admin/WorkflowCampaignController.php
+++ src/Controller/Admin/WorkflowCampaignController.php
@@ -8,31 +8,23 @@ use Abderrahim\SyliusWorkflowPlugin\Entity\WorkflowCampaign;
 use Abderrahim\SyliusWorkflowPlugin\Enum\WorkflowStatus;
 use Abderrahim\SyliusWorkflowPlugin\Form\Type\WorkflowCampaignType;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\Routing\RouterInterface;
-use Twig\Environment;
 
-#[AsController]
-final class WorkflowCampaignController
+final class WorkflowCampaignController extends AbstractController
 {
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
-        private readonly Environment $twig,
-        private readonly FormFactoryInterface $formFactory,
-        private readonly RouterInterface $router,
     ) {
     }
 
     public function create(Request $request): Response
     {
         $campaign = new WorkflowCampaign();
-        $form = $this->formFactory->create(WorkflowCampaignType::class, $campaign);
+        $form = $this->createForm(WorkflowCampaignType::class, $campaign);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -52,16 +44,12 @@ final class WorkflowCampaignController
             $this->entityManager->persist($campaign);
             $this->entityManager->flush();
 
-            return new RedirectResponse(
-                $this->router->generate('sylius_workflow_admin_canvas', ['id' => $campaign->getId()])
-            );
+            return $this->redirectToRoute('sylius_workflow_admin_canvas', ['id' => $campaign->getId()]);
         }
 
-        $content = $this->twig->render('@SyliusWorkflowPlugin/admin/workflow_campaign/create.html.twig', [
+        return $this->render('@SyliusWorkflowPlugin/admin/workflow_campaign/create.html.twig', [
             'form' => $form->createView(),
         ]);
-
-        return new Response($content);
     }
 
     public function editCanvas(int $id): Response
@@ -71,14 +59,12 @@ final class WorkflowCampaignController
             throw new NotFoundHttpException('Campaign not found.');
         }
 
-        $content = $this->twig->render('@SyliusWorkflowPlugin/admin/workflow_campaign/canvas.html.twig', [
+        return $this->render('@SyliusWorkflowPlugin/admin/workflow_campaign/canvas.html.twig', [
             'campaign' => $campaign,
         ]);
-
-        return new Response($content);
     }
 
-    public function delete(int $id, Request $request): Response
+    public function delete(int $id): Response
     {
         $campaign = $this->entityManager->find(WorkflowCampaign::class, $id);
         if ($campaign === null) {
@@ -88,7 +74,9 @@ final class WorkflowCampaignController
         $this->entityManager->remove($campaign);
         $this->entityManager->flush();
 
-        return new RedirectResponse($this->router->generate('workflow_admin_campaign_index'));
+        $this->addFlash('success', 'Workflow deleted successfully.');
+
+        return $this->redirectToRoute('workflow_admin_campaign_index');
     }
 
     public function toggleEnabled(int $id): JsonResponse
