d9242e1c2ba79248e74e260ced08a429a396bdcf
Abderrahim GHAZALI <ghazali.abderrahim1@gmail.com>
2026-04-06 21:15:24 +0200

    Revert "style: Shopify Flow-style condition branches for LTR layout"
    This reverts commit aa5f224219fcf4a2de74645ffe160a40246f5c1f.

--- assets/workflow-editor/nodes/BaseNode.jsx
+++ assets/workflow-editor/nodes/BaseNode.jsx
@@ -68,11 +68,11 @@ export default function BaseNode({ data, selected }) {
                 <div className="swp-node__branches">
                     <div className="swp-node__branch swp-node__branch--true">
                         <span className="swp-node__branch-label">Then</span>
-                        <Handle type="source" position={Position.Right} id="exit-true" />
+                        <Handle type="source" position={Position.Right} id="exit-true" style={{ top: '35%' }} />
                     </div>
                     <div className="swp-node__branch swp-node__branch--false">
                         <span className="swp-node__branch-label">Otherwise</span>
-                        <Handle type="source" position={Position.Right} id="exit-false" />
+                        <Handle type="source" position={Position.Right} id="exit-false" style={{ top: '65%' }} />
                     </div>
                 </div>
             ) : (
